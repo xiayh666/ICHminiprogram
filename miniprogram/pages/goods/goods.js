a7b63cb63444bba838743edd3ede3b94215e42ff
xiayh666 <3185979303@qq.com>
2025-11-17 11:38:49 +0800

    添加了tab-bar的icon 这里补充一点，由于非企业认证账号的限制，后端使用的云开发可能无法在每一个开发者的环境中使用，将app.js里的globalData.cloud 改为false 可以切换（其实只是this.getAsset这个简单的封装，目前过于简陋，仍需修改）

--- miniprogram/pages/goods/goods.js
+++ miniprogram/pages/goods/goods.js
@@ -3,28 +3,28 @@ Page({
     goodsList: [
       {
         id: 1,
-        img: "/images/goods/手提包.png", // 需替换为实际图片路径
+        img: "/images/goods/手提包.png",
         name: "夏布手提包",
         desc: "百年工艺 夏日清凉",
         price: 68
       },
       {
         id: 2,
-        img: "/images/goods/围巾.png", // 需替换为实际图片路径
+        img: "/images/goods/围巾.png",
         name: "夏布围巾",
         desc: "传统工艺手绘青花",
         price: 158
       },
       {
         id: 3,
-        img: "/images/goods/胸花.png", // 需替换为实际图片路径
+        img: "/images/goods/胸花.png",
         name: "夏布胸花",
         desc: "天然植物染色 手工制作",
         price: 259
       },
       {
         id: 4,
-        img: "/images/goods/玩偶.png", // 需替换为实际图片路径
+        img: "/images/goods/玩偶.png",
         name: "夏布老虎玩偶",
         desc: "大师手作 传世收藏",
         price: 280
@@ -33,6 +33,5 @@ Page({
   },
 
   onLoad: function () {
-    // 页面加载时的逻辑
   }
 })
